251226d37fcdc9b65a2c2e4dacdcd7c293ff9364
Andrey Garkavyy <andrewgarkavyy@gmail.com>
2015-12-18 17:35:50 +0300

    other tasks deleted

--- andrewgark/src/main/java/ru/fizteh/fivt/students/andrewgark/CollectionQuery/impl/Query.java
+++
@@ -1,8 +0,0 @@
-package ru.fizteh.fivt.students.andrewgark.CollectionQuery.impl;
-
-import java.lang.reflect.InvocationTargetException;
-
-public interface Query<R> {
-    Iterable<R> execute() throws NoSuchMethodException,
-            IllegalAccessException, InvocationTargetException, InstantiationException;
-}
